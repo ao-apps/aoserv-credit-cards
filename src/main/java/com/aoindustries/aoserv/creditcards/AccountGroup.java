110f1f6f95a49efd99e98d11d192d3b7f24dbeb4
AO Industries, Inc. <support@aoindustries.com>
2019-08-12 16:46:33 -0500

    Resolved javadoc warnings.

--- src/main/java/com/aoindustries/aoserv/creditcards/AccountGroup.java
+++ src/main/java/com/aoindustries/aoserv/creditcards/AccountGroup.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 /**
- * Uses a {@link Account} as a Java {@linux Group}.
+ * Uses a {@link Account} as a Java {@link Group}.
  *
  * @author  AO Industries, Inc.
  */
